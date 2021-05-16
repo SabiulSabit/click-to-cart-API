b7f94a4dbc3ab8bbf8910d626876c6bd1397d3a8
SabitTheBot <iamsabit99@gmail.com>
2021-05-16 22:22:02 +0600

    connect to db done

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express =  require('express');
+const mongoose = require('mongoose')
 require('dotenv').config();
 
+//app
 const app = express();
 
+//connect to db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then( () =>{ 
+    console.log("Connected To Database");
+} )
+   
+
+
+
+//routing
 app.get('/', (req,res,next)=>{
     res.send("hello world")
 })
