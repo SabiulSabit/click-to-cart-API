3b0e97d58853067c7143e2f8469fc2b9a2d8da6a
SabitTheBot <iamsabit99@gmail.com>
2021-05-17 16:08:58 +0600

    user schema create done

--- models/user.js
+++ models/user.js
@@ -1,2 +1,38 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlenght: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    hashPassword: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    history: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
