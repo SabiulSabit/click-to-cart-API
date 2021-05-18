51bdbf7dc2871c7e734c74c23574defb77cf16d3
SabitTheBot <iamsabit99@gmail.com>
2021-05-18 22:49:07 +0600

    add isAuth to controlers

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,9 +68,20 @@ exports.signup = (req,res,next) =>{
 
 
  //require sign in
-
 exports.requireSignin = expressJWT({
     secret: process.env.JWT_SECRET,
     algorithms: ["HS256"],
     userProperty: 'auth'
 })
+
+exports.isAuth = (req,res,next) =>{
+    let user = req.profile && req.auth && req.profile._id === req.auth._id;
+    if(!user){
+        return res.status(403).json({
+            error: "Access Denied"
+        })
+    }
+
+    next();
+}
+
