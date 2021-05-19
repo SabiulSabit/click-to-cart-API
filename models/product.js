35d59090f4e5643fe128f446fb3b7c1b6ce301e9
SabitTheBot <iamsabit99@gmail.com>
2021-05-19 21:23:31 +0600

    create product models

---
+++ models/product.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlenght: 32,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlenght: 2000,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      require: false,
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
