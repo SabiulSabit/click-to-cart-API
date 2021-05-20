602fef09f5aaca3e8106de71d2dacedd4513d662
SabitTheBot <iamsabit99@gmail.com>
2021-05-21 01:39:53 +0600

    product update done

--- controllers/product.js
+++ controllers/product.js
@@ -90,5 +90,48 @@ exports.remove = (req,res,next) =>{
 
 //update product
 exports.update = (req,res,next) =>{
-    
+    let form = new formidable.IncomingForm();
+
+    form.keepExtensions = true;
+
+    form.parse(req,(err,fields,files)=>{
+        if(err){
+            return res.status(400).json({
+                error: "Image Upload Error"
+            })
+        }
+      
+        const {name, description, price, category, quantity, shipping} = fields;
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error: "All Fields are Required"
+            })
+        }
+
+        let product = req.product;
+        product= _.extend(product, fields);
+
+        if(files.photo){
+
+            if(files.photo.size> 1000000){
+                return res.status(400).json({
+                    error: "Image Should be less than 1MB",
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result)=>{
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            return res.json({
+               result
+            })
+
+        })
+    })
 }
