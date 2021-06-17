6c646b9d7b520006579832c8b896c728a3767f7b
SabitTheBot <iamsabit99@gmail.com>
2021-06-18 05:38:46 +0600

    braintree token generate done

--- routes/braintree.js
+++ routes/braintree.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 const authController = require('../controllers/auth');
 const userController = require('../controllers/user');
-const 
+const braintreeController = require('../controllers/braintree'); 
 
-router.get('/braintree/getToken/:userId',authController.requireSignin, authController.isAuth,  )
+router.get('/braintree/getToken/:userId',authController.requireSignin, authController.isAuth,braintreeController.generateToken  )
 
 
 router.param('userId', userController.userByID);
