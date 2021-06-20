ffcc3f5a56b117d0a27327b9cf4354338f562017
SabitTheBot <iamsabit99@gmail.com>
2021-06-20 17:40:18 +0600

    ordder ceate code added

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,20 @@
+const {Order, CartItem} = require('../models/order');
+const {errorHandler} = require('../helpers/dbErrorHandler');
+
 
 //create new order
 exports.postCreatOrder = (req,res, next) => {
-  console.log(req.body)
+  req.body.order.user  = req.profile;
+
+  const order = new Order(req.body.order);
+
+  order.save( (err, result)=>{
+    if(err){
+      return res.status(400).json({
+        error: errorHandler(err)
+      })
+    }else{
+      return res.json(result);
+    }
+  })
 }
