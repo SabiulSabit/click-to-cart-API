316df561d37ed1bc4db6a2d185d04ef6b6d0b4d0
SabitTheBot <iamsabit99@gmail.com>
2021-06-20 18:58:39 +0600

    order list code added

--- controllers/order.js
+++ controllers/order.js
@@ -1,25 +1,35 @@
-const {Order, CartItem} = require('../models/order');
-const {errorHandler} = require('../helpers/dbErrorHandler');
-
+const { Order, CartItem } = require("../models/order");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 //create new order
-exports.postCreatOrder = (req,res, next) => {
-  req.body.order.user  = req.profile;
+exports.postCreatOrder = (req, res, next) => {
+  req.body.order.user = req.profile;
 
   const order = new Order(req.body.order);
 
-  order.save( (err, result)=>{
-    if(err){
+  order.save((err, result) => {
+    if (err) {
       return res.status(400).json({
-        error: errorHandler(err)
-      })
-    }else{
+        error: errorHandler(err),
+      });
+    } else {
       return res.json(result);
     }
-  })
-}
+  });
+};
 
 //get all order list for admin
-exports.listOrder = (req,res,next) =>{
-
-}
+exports.listOrder = (req, res, next) => {
+  Order.find()
+    .populate("user", "_id name address")
+    .sort("-created")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      } else {
+        return res.json(order);
+      }
+    });
+};
